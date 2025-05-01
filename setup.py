f732ffe78d0cec9207394e62dbaf76831b4387a2
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-05-01 19:25:09 +0000

    feat: added a setupfile for install in terminal (wanted to test it on larger repositories)

---
+++ setup.py
@@ -0,0 +1,34 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="gitwit",
+    version="0.1.0",
+    description="A CLI for showing activity, experts, etc.. based on git data.",
+    author="Jack Francis Dalton",
+    author_email="jackfrancisdalton@gmail.com",
+    python_requires=">=3.7",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "typer[all]>=0.9.0",
+        "GitPython>=3.1.31",
+        "rich>=13.5.2",
+        "watchdog>=4.0.0",
+    ],
+    # extras_require={ Don't think these are needed, will review later
+    #     "dev": [
+    #         "pytest>=7.3.1",
+    #         "pytest-mock>=3.10.0",
+    #     ],
+    # },
+    entry_points={
+        "console_scripts": [
+            "gitwit = src.cli:app",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Typer",
+    ],
+)
